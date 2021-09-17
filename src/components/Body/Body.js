2bd117db57aa3a9b89707b0af5ad39ad7185c41b
ericksonder <ericksonder017@gmail.com>
2021-09-16 20:02:32 -0500

    Añadiendo la seccion OurPublic

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import AboutUs from '../AboutUs/AboutUs'
 import Hero from '../Hero/Hero'
+import OurPublic from '../OurPublic/OurPublic'
 import WhyUs from '../WhyUs/WhyUs'
 
 const Body = () => {
     return (
-        <div>
+        <>
         <Hero />
         <AboutUs />
         <WhyUs />
+        <OurPublic />
             
-        </div>
+        </>
     )
 }
 
